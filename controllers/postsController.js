41805cc14a4fb24fa8411f6c553274d05bee83d2
liushaofei <liushaofei185@163.com>
2020-10-22 09:06:17 +0800

    文章数据展示细节--状态转换和日期转换

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,7 @@
 // 引入文章数据处理模块
 var postsModule = require('../modules/postsModule.js')
+// 引入moment
+// var moment = require('moment')
 exports.getPostList = (req,res) =>{
   // 获取所有文章列表并返回
   // 调用数据模块获取数据
@@ -10,6 +12,11 @@ exports.getPostList = (req,res) =>{
         msg:'数据查询失败'
       })
     } else {
+      // 将日期格式进行合理的转换
+      // for (var i=0;i<data.length; i++) {
+      //   // moment中的参数就是你想转换的日期值,如果没有写,就默认获取当前的日期值进行转换
+      //   data[i].created = moment(data[i].created).format('YYYY-MM-DD HH:mm:ss')
+      // }
       res.json({
         code: 200,
         msg: '数据查询成功',
--- modules/postsModule.js
+++ modules/postsModule.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'baixiu'
+  database: 'baixiu',
+  dateStrings: true   // 可以将数据库中的日期格式的数据读取之后进行合理的转换
 })
 // 打开连接--不用写,默认找到最新创建的连接来使用
 
